25814f82b96ea06b3a400201612bb3a569f150ff
scolak1 <scolak1@192.168.1.142>
2016-08-25 21:39:06 +0200

    Work on import of Excel. Now reads file, finds first row and builds dictionary of all first row headers.

---
+++ VolvoProjects/RPT/RPT/src/rpt/GUI/ProgramManager/Variants/Variant.java
@@ -0,0 +1,281 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpt.GUI.ProgramManager.Variants;
+
+/**
+ *
+ * @author SCOLAK1
+ */
+public class Variant {
+    String plant;
+    int plantCode;
+    String plantName;
+    String vehicle;
+    String typeCode;
+    String vehicleSOP;
+    String vehicleEOP;
+    String propulsion;
+    String denomination;
+    String fuel;
+    String engineFamily;
+    String generation;
+    String engineName;
+    String engineCode;
+    float displacement;
+    int enginePower;
+    int elMotorpower;
+    int torque;
+    int torqueOverboost;
+    char gearBoxType;
+    int gears;
+    String gearbox;
+    String driveline;
+    char transmissionCode;
+    String certGroup;
+    String Emiss;
+    String startOfProd;
+    String endOfProd;
+    
+    
+    public Variant(){
+        
+    }
+
+    public String getPlant() {
+        return plant;
+    }
+
+    public void setPlant(String plant) {
+        this.plant = plant;
+    }
+
+    public int getPlantCode() {
+        return plantCode;
+    }
+
+    public void setPlantCode(int plantCode) {
+        this.plantCode = plantCode;
+    }
+
+    public String getPlantName() {
+        return plantName;
+    }
+
+    public void setPlantName(String plantName) {
+        this.plantName = plantName;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getTypeCode() {
+        return typeCode;
+    }
+
+    public void setTypeCode(String typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public String getVehicleSOP() {
+        return vehicleSOP;
+    }
+
+    public void setVehicleSOP(String vehicleSOP) {
+        this.vehicleSOP = vehicleSOP;
+    }
+
+    public String getVehicleEOP() {
+        return vehicleEOP;
+    }
+
+    public void setVehicleEOP(String vehicleEOP) {
+        this.vehicleEOP = vehicleEOP;
+    }
+
+    public String getPropulsion() {
+        return propulsion;
+    }
+
+    public void setPropulsion(String propulsion) {
+        this.propulsion = propulsion;
+    }
+
+    public String getDenomination() {
+        return denomination;
+    }
+
+    public void setDenomination(String denomination) {
+        this.denomination = denomination;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getEngineFamily() {
+        return engineFamily;
+    }
+
+    public void setEngineFamily(String engineFamily) {
+        this.engineFamily = engineFamily;
+    }
+
+    public String getGeneration() {
+        return generation;
+    }
+
+    public void setGeneration(String generation) {
+        this.generation = generation;
+    }
+
+    public String getEngineName() {
+        return engineName;
+    }
+
+    public void setEngineName(String engineName) {
+        this.engineName = engineName;
+    }
+
+    public String getEngineCode() {
+        return engineCode;
+    }
+
+    public void setEngineCode(String engineCode) {
+        this.engineCode = engineCode;
+    }
+
+    public float getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(float displacement) {
+        this.displacement = displacement;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public int getElMotorpower() {
+        return elMotorpower;
+    }
+
+    public void setElMotorpower(int elMotorpower) {
+        this.elMotorpower = elMotorpower;
+    }
+
+    public int getTorque() {
+        return torque;
+    }
+
+    public void setTorque(int torque) {
+        this.torque = torque;
+    }
+
+    public int getTorqueOverboost() {
+        return torqueOverboost;
+    }
+
+    public void setTorqueOverboost(int torqueOverboost) {
+        this.torqueOverboost = torqueOverboost;
+    }
+
+    public char getGearBoxType() {
+        return gearBoxType;
+    }
+
+    public void setGearBoxType(char gearBoxType) {
+        this.gearBoxType = gearBoxType;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public String getGearbox() {
+        return gearbox;
+    }
+
+    public void setGearbox(String gearbox) {
+        this.gearbox = gearbox;
+    }
+
+    public String getDriveline() {
+        return driveline;
+    }
+
+    public void setDriveline(String driveline) {
+        this.driveline = driveline;
+    }
+
+    public char getTransmissionCode() {
+        return transmissionCode;
+    }
+
+    public void setTransmissionCode(char transmissionCode) {
+        this.transmissionCode = transmissionCode;
+    }
+
+    public String getCertGroup() {
+        return certGroup;
+    }
+
+    public void setCertGroup(String certGroup) {
+        this.certGroup = certGroup;
+    }
+
+    public String getEmiss() {
+        return Emiss;
+    }
+
+    public void setEmiss(String Emiss) {
+        this.Emiss = Emiss;
+    }
+
+    public String getStartOfProd() {
+        return startOfProd;
+    }
+
+    public void setStartOfProd(String startOfProd) {
+        this.startOfProd = startOfProd;
+    }
+
+    public String getEndOfProd() {
+        return endOfProd;
+    }
+
+    public void setEndOfProd(String endOfProd) {
+        this.endOfProd = endOfProd;
+    }
+    
+    public void setValue(String dataField, Object input){
+        switch(dataField){
+            case "Engine Name":
+                setEngineName((String) input);
+                break;
+            default:
+                break;
+        }
+    }
+    
+}
--- VolvoProjects/RPT/RPT/src/rpt/GUI/ProgramManager/Variants/VariantsController.java
+++ VolvoProjects/RPT/RPT/src/rpt/GUI/ProgramManager/Variants/VariantsController.java
@@ -7,9 +7,18 @@ package rpt.GUI.ProgramManager.Variants;
 
 import rpt.GUI.ProgramManager.*;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Observable;
 import java.util.ResourceBundle;
 import javafx.beans.property.Property;
@@ -42,6 +51,12 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 /**
  * FXML Controller class
@@ -114,8 +129,25 @@ public class VariantsController implements Initializable {
         tableVariants.getItems().removeAll(removeVariants);
     }        
     
+    private Object getCellValue(Cell cell) {
+        switch (cell.getCellType()) {
+        case Cell.CELL_TYPE_STRING:
+            return cell.getStringCellValue();
+ 
+        case Cell.CELL_TYPE_BOOLEAN:
+            return cell.getBooleanCellValue();
+ 
+        case Cell.CELL_TYPE_NUMERIC:
+            return cell.getNumericCellValue();
+        }
+ 
+        return null;
+    }
+    
     //Import button action handler
     public void importButtonClicked(ActionEvent event) throws IOException {
+        //List with all variants read from the imported Excel file
+        List<Variant> variants = new ArrayList();
         
         //Create File Chooser window
         FileChooser fileChooser = new FileChooser();
@@ -129,8 +161,99 @@ public class VariantsController implements Initializable {
         File selectedFile = fileChooser.showOpenDialog(null);
         
         //import Excel file if a file has been selected, if not, do nothing
+        //based on good example on:
+        //http://www.codejava.net/coding/how-to-read-excel-files-in-java-using-apache-poi
         if (selectedFile != null) {
-            System.out.println("File selected: " + selectedFile.getName());
+            System.out.println("File selected: " + selectedFile.getPath());
+            FileInputStream inputStream = new FileInputStream(new File(selectedFile.getPath()));
+            Workbook workbook = new HSSFWorkbook(inputStream);
+            
+            //Use Sheet iterator to extract all sheet names
+            Iterator<Sheet> sheetIterator = workbook.sheetIterator();
+            
+            //Iterate over all sheets and populate a checkboxfield and let user select on of the sheets
+            while (sheetIterator.hasNext()){
+                Sheet nextSheet = sheetIterator.next();
+                //TODO
+                //Create dialogbox with a radio button selection showing all the available sheets.
+                //Clicking on cancel has to break the main if or protect the rest with an if that Cancel wasnt clicked
+            }
+            
+            //set sheet to the sheet selected
+            Sheet firstSheet = workbook.getSheet("Raw data CP2016A"); //Change to variable name at later date
+            Iterator<Row> rowIterator = firstSheet.iterator();
+            
+            //find first row
+            //TODO
+            //set first row keyword into application settings
+            Boolean firstRowFound = false;
+            Cell nextCell;
+            Iterator<Cell> cellIterator;
+            do{
+                Row nextRow = rowIterator.next();
+                cellIterator = nextRow.cellIterator();
+                nextCell = cellIterator.next();
+                if (getCellValue(nextCell) != null){//blank cells return null
+                    if (getCellValue(nextCell).equals("Plant")) {
+                        firstRowFound = true;
+                    }
+                }
+                
+            }while(!firstRowFound && rowIterator.hasNext());
+            
+            //First row is now found, loop through entire row and build a
+            //dictionary using HashMaps
+            Map<Integer, String> dictionary = new HashMap<Integer, String>();
+            while(cellIterator.hasNext()){
+                if(getCellValue(nextCell)!= null){ //blank cells return null
+                    dictionary.put(nextCell.getColumnIndex(), (String) getCellValue(nextCell));
+                }
+                nextCell = cellIterator.next();
+            }
+            
+            //loop through all rows in the file
+            while (rowIterator.hasNext()) {
+                Row nextRow = rowIterator.next();
+                cellIterator = nextRow.cellIterator();
+                Variant aVariant = new Variant();
+                    
+                //loop through all columns in the row
+                while (cellIterator.hasNext()) {
+                    nextCell = cellIterator.next();
+                    int columnIndex = nextCell.getColumnIndex();
+                    if (getCellValue(nextCell) != null) {
+                        aVariant.setValue(dictionary.get(nextCell.getColumnIndex()),getCellValue(nextCell));
+//                        switch (columnIndex) {
+//                            case 0:
+//                                aVariant.setPlant((String) getCellValue(nextCell));
+//                                break;
+//                            case 1:
+//                                Double value = ((Double) getCellValue(nextCell));
+//                                aVariant.setPlantCode(value.intValue());
+//                                break;
+//                            case 2:
+//                                aVariant.setPlantName((String) getCellValue(nextCell));
+//                                break;
+//                            default:
+//                                break;      
+//                        }
+                    } else {
+                    }
+                    
+                }
+                variants.add(aVariant);
+            }       
+            inputStream.close();
+            
+            //now that we have a list of Variants we put them into the tableView
+            data.clear();
+            int index = 1;
+            for (Variant variant : variants){
+                TableVariants entry = new TableVariants(index, variant.getEngineName(), variant.getDenomination(), variant.getGearbox(), variant.getEmiss());
+                add(entry);
+                index++;
+            }
+            
         }//TODO remove the else, we don't do anything if the user presses Cancel
         else {
             System.out.println("File selection cancelled.");
